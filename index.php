f4f37d4d1fb259739e9646bc21c573f980a75054
alex smolin <asmolin51@gmail.com>
2018-04-11 16:39:34 +0300

    Реализован интерфейс Countable. В шаблоне index.tmpl показывается общее количество новостей

--- index.php
+++ index.php
@@ -4,17 +4,10 @@ require __DIR__ . '/autoload.php';
 
 $view = new \App\Models\View();
 
-var_dump(\App\Models\Article::findAll());
 $view->articles = \App\Models\Article::findAll() ;
-
-
-//$author = \App\Models\Author::findById(3);
-
-
 
 $view->display(__DIR__ . '/App/Templates/index.tmpl');
 
-//require_once('App/Templates/index.tmpl');
 
 
 
