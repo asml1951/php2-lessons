404458535e021e24594ee31d5aa507c0ff5596ff
smolin <smolin@rivreg.ru>
2018-09-26 15:49:39 +0300

    Добавил View в article.php

--- App/Templates/article.tmpl.php
+++ App/Templates/article.tmpl.php
@@ -1,14 +1,14 @@
 <html>
 <head>
-    <title> Новость № <?php echo $article->id; ?></title>
+    <title> Новость № <?php echo $this->article->id; ?></title>
 </head>
 <body>
 
-<div><h1>Новость № <?php echo $article->id; ?></h1></div>
-<div><h3> <?= $article->title; ?></h3></div>
-<div><h3> <?= 'Автор: ' . $article->author->first_name . ' '
-                        . $article->author->last_name ; ?></h3></div>
-     <article><?php echo $article->content;  ?></article>
+<div><h1>Новость № <?php echo $this->article->id; ?></h1></div>
+<div><h3> <?= $this->article->title; ?></h3></div>
+<div><h3> <?= 'Автор: ' . $this->article->author->first_name . ' '
+                        . $this->article->author->last_name ; ?></h3></div>
+     <article><?php echo $this->article->content;  ?></article>
 
 </body>
 
--- article.php
+++ article.php
@@ -1,9 +1,11 @@
 <?php
 
+
 require __DIR__ . '/autoload.php';
 
 $id = $_GET['id'];
+$view = new \App\View();
+$view->article = \App\Models\Article::findById($id);
 
-$article = \App\Models\Article::findById($id);
+$view->display(__DIR__ . '/App/Templates/article.tmpl.php');
 
-require_once('App/Templates/article.tmpl.php');
